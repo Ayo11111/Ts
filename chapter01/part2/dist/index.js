fe6f06b818d036513c90834f760cf7ed1c1070ab
luoJie <1299124551@qq.com>
2022-08-03 14:50:44 +0800

    ✨ feat: 08（编译器配置项完结）

--- chapter01/part2/dist/index.js
+++ chapter01/part2/dist/index.js
@@ -1,1 +1,8 @@
 let c = 2;
+function fn2() {
+    alert(this);
+}
+let box1 = document.getElementById('box1');
+box1 === null || box1 === void 0 ? void 0 : box1.addEventListener('click', function () {
+    alert('hello');
+});
--- chapter01/part2/src/index.ts
+++ chapter01/part2/src/index.ts
@@ -1,1 +1,10 @@
 let c = 2
+
+function fn2() {
+    alert(this)
+}
+
+let box1 = document.getElementById('box1')
+box1?.addEventListener('click', function () {
+    alert('hello')
+})
